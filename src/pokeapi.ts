cacf576b6ce632316195222999a048b56872dfa3
Andrés Obón <andresdavidobon@gmail.com>
2025-07-22 14:22:34 -0500

    App retrieves data from cache when available instead of requesting API

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,38 +1,49 @@
-import type { ShallowLocations, Location } from "./pokeapi_types";
-import { Cache } from "./pokecache";
+import type { ShallowLocations, Location } from "./pokeapi_types.js";
+import { Cache } from "./pokecache.js";
 
 export class PokeAPI {
     private static readonly baseURL = "https://pokeapi.co/api/v2";
+    private cache;
 
-    constructor() {}
+    constructor() {
+        this.cache = new Cache(60000);
+    }
 
     async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-
         if (!pageURL) {
             pageURL = `${PokeAPI.baseURL}/location-area`;
         }
 
-        
+        const cacheEntry = this.cache.get<ShallowLocations>(pageURL);
+        if (cacheEntry && cacheEntry.val) {
+            return cacheEntry.val;
+        }
 
         const response = await fetch(pageURL, { method: "GET" });
-
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const parsedResponse = await response.json();
+        this.cache.add(pageURL, parsedResponse as ShallowLocations);
         return parsedResponse as ShallowLocations;
     }
 
     async fetchLocation(locationName: string): Promise<Location> {
+        const pageURL = `https://pokeapi.co/api/v2/location-area/${locationName}`;
 
-        const response = await fetch(`https://pokeapi.co/api/v2/location-area/${locationName}`, { method: "GET" });
-        
+        const cacheEntry = this.cache.get<Location>(pageURL);
+        if (cacheEntry && cacheEntry.val) {
+            return cacheEntry.val;
+        }
+
+        const response = await fetch(pageURL, { method: "GET" });
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const parsedResponse = await response.json();
+        this.cache.add(pageURL, parsedResponse as Location);
         return parsedResponse as Location;
     }
 }
